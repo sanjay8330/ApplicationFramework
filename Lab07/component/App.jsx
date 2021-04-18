a134093e2c125e01bb1136abcce724204b55bb01
sanjay8330 <sanjayabi8330@gmail.com>
2021-04-18 16:28:55 +0530

    The Lab07 initial commit 2

--- Lab07/component/App.jsx
+++ Lab07/component/App.jsx
@@ -1,23 +1,34 @@
-import React from 'react';
-import post from './Posts'; 
+//import React from 'react';
+//import Posts from '../component/Posts'; 
+
+import React from 'react'; 
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'; 
+import PostsHolder from '../component/PostsHolder';
+
 export default class App extends React.Component { 
     constructor(props) { 
         super(props); 
     } 
-    
     render() { 
-        return <div> 
-            <Posts posts={posts}/> 
-        </div>
-    }
+        return <Router> 
+            <Switch> 
+                <Route exact path="/"> 
+                    <PostsHolder/> 
+                </Route> 
+            </Switch> 
+        </Router> 
+    };
 }
 
-const posts = [ { 
+const posts = [ 
+    { 
     id: 1, 
     name: 'React', 
     description: 'Best UI library' 
-}, {
+    }, 
+    {
      id: 2, 
      name: 'Node',
-     description: 'Server side JS' } 
+     description: 'Server side JS' 
+    } 
 ];
--- Lab07/component/Posts.jsx
+++ Lab07/component/Posts.jsx
@@ -4,6 +4,14 @@ import PostListItem from './PostListItem';
 export default class Posts extends React.Component {
     constructor(props) { 
         super(props); 
+        this.state = {
+             post: null 
+        };
+    }
+
+    //New Method to set the aded post to the Posts component
+    selectPost(post) { 
+        this.setState({post: post}) 
     }
 
     render() { 
@@ -15,12 +23,15 @@ export default class Posts extends React.Component {
                         <th>ID</th>
                         <th>Name</th> 
                         <th>Description</th> 
+                        <th>Select Post</th>
                     </tr> 
                 </thead> 
                 <tbody> {posts.map(post => { 
-                    return <PostListItem key={post.id.toString()} post={post}/> })} 
+                    return <PostListItem key={post.id.toString()} post={post}/>
+                    })} 
                 </tbody> 
             </table> 
+            <div> {this.state.post ? <Post post={this.state.post} editable={false}/> : ''} </div>
         </div> 
     };
 }
